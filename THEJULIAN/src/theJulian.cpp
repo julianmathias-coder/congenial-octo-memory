ec24f3b1dfeb054e6cc395a6cb7d78eb54dd56b1
Julian Mathias <julianmathias@Julians-MacBook-Pro.local>
2024-12-03 19:11:24 -0700

    ultrSonic

--- THEJULIAN/src/theJulian.cpp
+++ THEJULIAN/src/theJulian.cpp
@@ -12,18 +12,41 @@
 #include "DFRobot_HumanDetection.h"
 #include "stdio.h"
 #include "application.h"
+#include <Adafruit_MQTT.h>
+#include "Adafruit_MQTT/Adafruit_MQTT_SPARK.h"
+#include "Adafruit_MQTT/Adafruit_MQTT.h"
+#include "credentials.h"
+
+/************ Global State (you don't need to change this!) ******************/ 
+TCPClient TheClient; 
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details. 
+Adafruit_MQTT_SPARK mqtt(&TheClient,AIO_SERVER,AIO_SERVERPORT,AIO_USERNAME,AIO_KEY); 
+
+/****************************** Feeds ***************************************/ 
+// Setup Feeds to publish or subscribe 
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>//must include /feeds/ before the feedname!
+Adafruit_MQTT_Publish mmwave = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/mmwave");
+
+void MQTT_connect();
+bool MQTT_ping();
+
+unsigned int last,lastTime;
 
 DFRobot_HumanDetection hu(&Serial1);
-SYSTEM_MODE(SEMI_AUTOMATIC);
+
+
+
+SYSTEM_MODE(AUTOMATIC);
 
 void setup() {
   Serial.begin(9600);
   waitFor(Serial.isConnected, 10000);
   Serial1.begin(115200);
 
-  WiFi.on();
-  WiFi.connect();
-  while (WiFi.connecting()) {
+   WiFi.on();
+   WiFi.connect();
+   while (WiFi.connecting()) {
     Serial.printf(".");
  }
     Serial.printf("\n\n");
@@ -74,6 +97,9 @@ void setup() {
 }
 
 void loop() {
+ MQTT_connect();
+ MQTT_ping();
+
   Serial.print("Existing information:");
   switch (hu.smHumanData(hu.eHumanPresence)) {
     case 0:
@@ -81,6 +107,7 @@ void loop() {
       break;
     case 1:
       Serial.println("Someone is present");
+      //digitalWrite()
       break;
     default:
       Serial.println("Read error");
@@ -109,4 +136,46 @@ void loop() {
   Serial.println(hu.getHeartRate());
   Serial.println("-----------------------");
   delay(1000);
+
+if((millis() - lastTime > 1000)) {  //Need to not overload Adafruit Dashboard
+    mmwave.publish((hu.smHumanData(hu.eHumanMovingRange))); //excluding time between pulses to simplify
+   // Serial.printf("Published movement: %0.1f RPM\n", RPMCalc);
+}
+    lastTime = millis(); // updated LastTime after publishing to Adafruit
+  }
+
+
+void MQTT_connect() {
+  int8_t ret;
+ 
+  // Return if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+ 
+  Serial.print("Connecting to MQTT... ");
+ 
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.printf("Error Code %s\n",mqtt.connectErrorString(ret));
+       Serial.printf("Retrying MQTT connection in 5 seconds...\n");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds and try again
+  }
+  Serial.printf("MQTT Connected!\n");
+}
+
+bool MQTT_ping() {
+  static unsigned int last;
+  bool pingStatus;
+
+  if ((millis()-last)>120000) {
+      Serial.printf("Pinging MQTT \n");
+      pingStatus = mqtt.ping();
+      if(!pingStatus) {
+        Serial.printf("Disconnecting \n");
+        mqtt.disconnect();
+      }
+      last = millis();
+  }
+  return pingStatus;
 }
---
+++ ultrasonic/src/ultrasonic.cpp
@@ -0,0 +1,35 @@
+/* 
+ * Project myProject
+ * Author: Your Name
+ * Date: 
+ * For comprehensive documentation and examples, please visit:
+ * https://docs.particle.io/firmware/best-practices/firmware-template/
+ */
+
+// Include Particle Device OS APIs
+#include "Particle.h"
+#include "HC-SR04.h"
+
+
+const int TRIG_PIN = D12;
+const int ECHO_PIN = D14;
+
+HC_SR04 rangefinder = HC_SR04(TRIG_PIN, ECHO_PIN);
+
+SYSTEM_MODE(SEMI_AUTOMATIC);
+
+
+
+void setup() {
+ rangefinder.init();
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+  // Example: Publish event to cloud every 10 seconds. Uncomment the next 3 lines to try it!
+  // Log.info("Sending Hello World to the cloud!");
+  // Particle.publish("Hello world!");
+  // delay( 10 * 1000 ); // milliseconds and blocking - see docs for more info!
+}
